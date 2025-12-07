3d5fc0a89babc1481a549d1f1c9ac3b91e0ea5fa
dbvzk <dmytrii.bevziuk@difreight.com>
2025-12-07 16:35:09 +0200

    first test

---
+++ tests/example.spec.ts
@@ -0,0 +1,10 @@
+import { test, expect } from '@playwright/test';
+
+test("test", async ({ page }) => {
+  await page.goto('https://dvbzk.github.io/test-app/');
+  await expect(page).toHaveTitle(/test-app/);
+});
+
+
+
+
