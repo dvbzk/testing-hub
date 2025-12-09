07e3de64572bbe248d98781001182eba2cd2198d
dbvzk <dmytrii.bevziuk@difreight.com>
2025-12-10 00:27:51 +0200

    change tests

--- tests/smoke/example.spec.ts
+++ tests/smoke/example.spec.ts
@@ -2,7 +2,7 @@ import { test, expect } from '@playwright/test';
 
 test("test", async ({ page }) => {
   await page.goto('https://dvbzk.github.io/test-app/');
-  await expect(page).toHaveTitle(/test-app/);
+  await expect(page).toHaveTitle(/test/);
 });
 
 
